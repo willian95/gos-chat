4fe6cf84af7564ae39ece2c04c8fb0ae317c597b
willian jesus rodriguez ramirez <rodriguezwillian95@gmail.com>
2020-08-16 14:48:38 -0400

    cambios

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ io.on('connection', function (socket) {
 
     let sender = message.sender_id
     let receiver = message.receiver_id
+    let transaction_id = message.transaction_id
     let prefix = ""
 
     if(receiver < sender)
@@ -15,7 +16,7 @@ io.on('connection', function (socket) {
     else
       prefix = sender+"-"+receiver
 
-    io.emit("message-"+prefix, {message: message.message, sender_id: message.sender_id, receiver_id: message.receiver_id, time: message.time})
+    io.emit("message-"+transaction_id+"-"+prefix, {message: message.message, sender_id: message.sender_id, receiver_id: message.receiver_id, time: message.time})
   })
  
 });
